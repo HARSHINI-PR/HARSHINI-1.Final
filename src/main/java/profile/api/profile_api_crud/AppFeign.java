d9fc00dfa97a8e5173d020e10dadcf2eb8c294e0
HARSHINI-PR <149145601+HARSHINI-PR@users.noreply.github.com>
2024-07-14 21:47:54 +0530

    FINAL

---
+++ src/main/java/profile/api/profile_api_crud/AppFeign.java
@@ -0,0 +1,15 @@
+package app.api.app_api_crud;
+
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "appService", url = "http://localhost:8087")
+public interface AppFeign {
+
+    @GetMapping("/profile/byApp/{appId}")
+    List<Profile> callProfile(@PathVariable("appId") int appId);
+}
